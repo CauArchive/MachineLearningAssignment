81cb8da6c3788b201fb9f460f85c5b751054d28b
woodi97 <woodi.daily@gmail.com>
2022-04-06 16:40:06 +0900

    Define: function construct_vector_b

--- notepad.py
+++ notepad.py
@@ -3,7 +3,12 @@ import numpy as np
 # a = np.arange(15).reshape(3, 5)
 # print(a)
 
-b = np.arange(5)
-print(b)
+# b = np.arange(5)
+# print(b)
 
-print(np.power(b, np.arange(len(b))));
+# print(np.power(b, np.arange(len(b))));
+
+n = 4
+b = np.zeros([n, 1])
+
+print(b)
