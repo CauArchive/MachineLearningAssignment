d29cd32a80f1e3137c88130d9f3bb31ee4b15361
woodi97 <woodi.daily@gmail.com>
2022-04-06 17:29:15 +0900

    Define: function solve_regression_with_regularization

--- notepad.py
+++ notepad.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-a = np.arange(6).reshape(2, 3)
-a = np.power(a, 2)
-print(np.min(np.sum(a, axis=1)/3))
+# a = np.arange(6).reshape(2, 3)
+# a = np.power(a, 2)
+# print(np.min(np.sum(a, axis=1)/3))
 
+a = np.eye(4)
+print(a)
