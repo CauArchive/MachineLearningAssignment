02169c27d57527db2fa6f191efa2b0b36e7581a9
woodi97 <woodi.daily@gmail.com>
2022-05-09 00:14:44 +0900

    Modify: function compute residual(add shape)

--- notepad.py
+++ notepad.py
@@ -1,12 +1,18 @@
 import numpy as np
 
-p = 3
-x = [1, 2, 3, 4, 5, 6, 7, 8]
-n = len(x)
-A = np.zeros([n, p])
-for i in range(p):
-    A[:, i] = x
-print(A)
-B = np.array([1, 2, 3])
-C = B * A
-print(C)
+# p = 3
+# x = [1, 2, 3, 4, 5, 6, 7, 8]
+# n = len(x)
+# A = np.zeros([n, p])
+# for i in range(p):
+#     A[:, i] = x
+# print(A)
+# B = np.array([1, 2, 3])
+# C = B * A
+# print(C)
+
+
+K = np.array([1, 2, 10])
+L = np.array([1, 2, 3])
+M = K * L
+print(1 - K)
