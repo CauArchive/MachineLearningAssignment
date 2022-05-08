f0bfce84d6be076d07492090cf9ea3e7551b513c
woodi97 <woodi.daily@gmail.com>
2022-05-09 00:11:20 +0900

    Modify: function linear regression(modify point)

--- notepad.py
+++ notepad.py
@@ -1,13 +1,12 @@
 import numpy as np
 
 p = 3
-x = [1,2,3,4,5,6,7,8]
+x = [1, 2, 3, 4, 5, 6, 7, 8]
 n = len(x)
 A = np.zeros([n, p])
 for i in range(p):
-  A[:,i] = x
+    A[:, i] = x
 print(A)
-A = np.power(A,np.arange(p))
-print(A)
-# result = np.power(x, np.arange(p))
-# print(result)
+B = np.array([1, 2, 3])
+C = B * A
+print(C)
